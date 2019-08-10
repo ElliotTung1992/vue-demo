a916fdfdfd5780599fefc34f6cbca273770a9a25
dge1992 <dge_1992@163.com>
2019-08-10 21:36:07 +0800

    第二次提交

--- pos/src/components/page/Pos.vue
+++ pos/src/components/page/Pos.vue
@@ -1,27 +1,215 @@
 <template>
   <div class="pos">
+    <div>
+      <el-row>
+        <el-col :span='7' class="pos-order" id="order-list">
+          <el-tabs>
+            <el-tab-pane label="点餐">
+              <el-table :data="tableData" border show-summary style="width: 100%" >
+                <el-table-column prop="goodsName" label="商品"  ></el-table-column>
+                <el-table-column prop="count" label="量" width="50"></el-table-column>
+                <el-table-column prop="price" label="金额" width="70"></el-table-column>
+                <el-table-column  label="操作" width="100" fixed="right">
+                  <template scope="scope">
+                    <el-button type="text" size="small">删除</el-button>
+                    <el-button type="text" size="small">增加</el-button>
+                  </template>
+                </el-table-column>
+              </el-table>
+              <div class="order-btn">
+                <el-button type="warning" >挂单</el-button>
+                <el-button type="danger" >删除</el-button>
+                <el-button type="success" >结账</el-button>
+              </div>
+            </el-tab-pane>
+            <el-tab-pane label="挂单">
+              挂单
+            </el-tab-pane>
+            <el-tab-pane label="外卖">
+              外卖
+            </el-tab-pane>
+          </el-tabs>
+        </el-col>
+        <!--商品展示-->
+        <el-col :span="17">
+
+          <div class="often-goods">
+            <div class="title">常用商品</div>
+            <div class="often-goods-list">
+              <ul>
+                <li v-for="goods in oftenGoods" @click="addOrderList(goods)">
+                  <span>{{goods.goodsName}}</span>
+                  <span class="o-price">￥{{goods.price}}元</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="goods-type">
+            <el-tabs>
+              <el-tab-pane label="汉堡">
+                <ul class='cookList'>
+                  <li v-for="goods in type0Goods">
+                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                    <span class="foodName">{{goods.goodsName}}</span>
+                    <span class="foodPrice">￥{{goods.price}}元</span>
+                  </li>
+                </ul>
+              </el-tab-pane>
+              <el-tab-pane label="小食">
+                <ul class='cookList'>
+                  <li v-for="goods in type1Goods">
+                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                    <span class="foodName">{{goods.goodsName}}</span>
+                    <span class="foodPrice">￥{{goods.price}}元</span>
+                  </li>
+                </ul>
+              </el-tab-pane>
+              <el-tab-pane label="饮料">
+                <ul class='cookList'>
+                  <li v-for="goods in type2Goods">
+                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                    <span class="foodName">{{goods.goodsName}}</span>
+                    <span class="foodPrice">￥{{goods.price}}元</span>
+                  </li>
+                </ul>
+              </el-tab-pane>
+              <el-tab-pane label="套餐">
+                <ul class='cookList'>
+                  <li v-for="goods in type3Goods">
+                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                    <span class="foodName">{{goods.goodsName}}</span>
+                    <span class="foodPrice">￥{{goods.price}}元</span>
+                  </li>
+                </ul>
+              </el-tab-pane>
+            </el-tabs>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'Pos'
+  name: 'Pos',
+  data(){
+    return {
+      tableData: [{
+        goodsName: '可口可乐',
+        price: 8,
+        count:1
+      }, {
+        goodsName: '香辣鸡腿堡',
+        price: 15,
+        count:1
+      }, {
+        goodsName: '爱心薯条',
+        price: 8,
+        count:1
+      }, {
+        goodsName: '甜筒',
+        price: 8,
+        count:1
+      }],
+      oftenGoods:[],
+      type0Goods:[],
+      type1Goods:[],
+      type2Goods:[],
+      type3Goods:[],
+    }
+  },
+  created: function(){
+    //获取常用商品数据
+    axios.get('https://www.easy-mock.com/mock/5b8b30dbf032f03c5e71de7f/kuaican/oftenGoods')
+      .then(res => {
+        this.oftenGoods=res.data;
+      })
+      .catch(error=>{
+        alert('网络错误，不能访问');
+      });
+    //读取分类商品列表
+    axios.get('https://www.easy-mock.com/mock/5b8b30dbf032f03c5e71de7f/kuaican/typeGoods')
+      .then(response=>{
+        this.type0Goods=response.data[0];
+        this.type1Goods=response.data[1];
+        this.type2Goods=response.data[2];
+        this.type3Goods=response.data[3];
+      })
+      .catch(error=>{
+        alert('网络错误，不能访问');
+      })
+  },
+  mounted: function(){
+    let orderHeight = document.body.clientHeight;
+    document.getElementById("order-list").style.height = orderHeight + 'px';
+  },
 }
 </script>
 
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .pos {
+    font-size: 12px;
+  }
+  .pos-order {
+    background-color: #F9FAFC;
+    border-right: 1px solid #C0CCDA;
+  }
+  .order-btn {
+    margin-top: 10px;
+    text-align: center;
+  }
+  .title {
+    height: 20px;
+    border-bottom: 1px solid #D3DCE6;
+    background-color: #F9FAFC;
+    padding: 10px;
+  }
+  .often-goods-list ul li{
+    list-style: none;
+    float:left;
+    border:1px solid #E5E9F2;
+    padding:10px;
+    margin:5px;
+    background-color:#fff;
+  }
+  .o-price{
+    color:#58B7FF;
+  }
+  .goods-type {
+    clear: both;
+  }
+  .cookList li{
+    list-style: none;
+    width:23%;
+    border:1px solid #E5E9F2;
+    height: auot;
+    overflow: hidden;
+    background-color:#fff;
+    padding: 2px;
+    float:left;
+    margin: 2px;
+
+  }
+  .cookList li span{
+
+    display: block;
+    float:left;
+  }
+  .foodImg{
+    width: 40%;
+  }
+  .foodName{
+    font-size: 18px;
+    padding-left: 10px;
+    color:brown;
+
+  }
+  .foodPrice{
+    font-size: 16px;
+    padding-left: 10px;
+    padding-top:10px;
+  }
 </style>
