4523cccc328d17c809503db6ef49dc516ff655ed
dge1992 <dge_1992@163.com>
2019-08-13 22:08:29 +0800

    vue route练习

---
+++ vue_test/src/components/Error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hi" style="color: darkred">
+    {{msg}}
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hi',
+  data () {
+    return {
+      msg: '404 页面丢失了！'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue_test/src/components/Param.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{msg}} - {{$route.params.id}} - {{$route.params.name}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Param',
+  data () {
+    return {
+      msg: 'i am Param'
+    }
+  },
+  beforeRouteEnter:(to,from,next)=>{
+    console.log("准备进入Param模板");
+    next();
+  },
+  beforeRouteLeave: (to, from, next) => {
+    console.log("准备离开Param模板");
+    next();
+  }
+}
+</script>
+
+<style scoped>
+</style>
